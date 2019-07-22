eb14ee87b1fad999b30176f79e3ef6d22c300d10
Edilson Silva <edilson_js@yahoo.com.br>
2019-07-22 09:16:59 -0300

    Realizando Cálculos

--- Program.cs
+++ Program.cs
@@ -10,11 +10,17 @@ namespace Tabuada
            //Limpar a tela de console
            Console.Clear();
            Console.WriteLine("############################################################");
-           Console.WriteLine("#                 Programa de tabuda                       #");
+           Console.WriteLine("#                 Programa de tabuada                      #");
            Console.WriteLine("############################################################");
            Console.WriteLine();
            Console.WriteLine(" Digite um número > ");
-           
+           numero = int.Parse(Console.ReadLine());
+
+
+           for(int x = 0; x <= 10; x++){
+               Console.WriteLine(numero+" x "+x+" = "+numero*x);
+           }
+
         }
     }
 }
